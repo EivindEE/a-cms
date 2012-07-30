2ffaa08bdf267ed0947908d787fc3006364bb075
Mathias M. Stav <mathias@brafolk.no>
2012-07-30 23:40:16 +0200

    Added Jade and Express and set this up in app.js. managed to get content from database and use it with a jade template with an included header.

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-var http = require('http'),
-	mongoose = require('mongoose'),	
-	db = mongoose.connect('mongodb://localhost/nodeblog');
-	
-var Schema = mongoose.Schema
-  , ObjectId = Schema.ObjectId;
+
+var app = require('express').createServer()
+, mongoose = require('mongoose')
+, db = mongoose.connect('mongodb://localhost/nodeblog')
+, Schema = mongoose.Schema
+, ObjectId = Schema.ObjectId;
+
+//Setup express
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
 //Define a scema
 var page = new Schema({
-    author    : ObjectId
-  , title     : String
-  , slug     : String
-  , content      : String
-  , buf       : Buffer	//Don't know how this works
-  , date      : Date	//Don't know how this works
+    author	: ObjectId
+  , title	: String
+  , slug	: String
+  , content	: String
+  , buf		: Buffer	//Don't know how this works
+  , date	: Date	//Don't know how this works
 });
 
-var MyModel = db.model('pages', page);
+var pages = db.model('pages', page);
 
-function createNewInstance(){
-	var instance = new MyModel();
-	instance.title = 'This is a page';
-	instance.slug = "testpage";
+function createNewPage(){
+	var instance = new pages();
+	instance.title = 'This is a post';
+	instance.slug = "myfirstpost";
 	instance.content = "Proin vestibulum. Ut ligula. Nullam sed dolor id odio volutpat pulvinar. Integer a leo. In et eros at neque pretium sagittis. Sed sodales lorem a ipsum suscipit gravida. Ut fringilla placerat arcu. Phasellus imperdiet. Mauris ac justo et turpis pharetra vulputate";
 	
 	instance.save(function (err) {
@@ -31,38 +37,28 @@ function createNewInstance(){
 		}
 	});		
 }
-
-//createNewInstance();
+	
+//createNewPage();
 
 
 function getTitleBySlug($slug, $cb){
-	MyModel.find({slug: $slug}, function(err, docs) {
-	  	docs.forEach(function(elem, index, array){
-		  	console.log(elem.title);
-		  	$cb(elem.title); 
+	pages.find({slug: $slug}, function(err, data) {
+	  	data.forEach(function(elem, index, array){
+		  	$cb(elem); 
 	  	});
 	});	
 }
 
+app.get('/',function(req, res){
+	getTitleBySlug('myfirstpost', function(data) { 
+		console.log(data);
+		res.render('index', {data: data});
+	})
+});
 
 
-var server = http.createServer(function(request, response){
-	
-	
-	getTitleBySlug('testpage', function(val){
-		response.writeHead(200, {
-			"Content" : "text/html"
-		});
-		response.write('<html><head><title>'+val+'</title></head><body>');
-		response.write('<h1>'+val+'</h1>');
-		response.write('<p>'+val+'</p>');
-		response.write('</body></html>');
-		response.end();
-	});
-	
-	
-})
 
-server.listen(8000, 'nodeblog.lo');
+
+app.listen(8000);
 
 console.log('Listening on http://127.0.0.1:8000');
